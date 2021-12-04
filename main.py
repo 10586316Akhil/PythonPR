1e32b933b81275ba6fa11f73c609b5c372d48370
Akhil Shirole <shirole.akhil@gmail.com>
2021-12-04 19:37:58 +0000

    Net deduction

--- main.py
+++ main.py
@@ -28,3 +28,50 @@ class Employee:
         HigherPay = 0
         if(GrossPay > RegularPay):
             HigherPay = GrossPay - self.StandardBand
+            
+
+        StdTax = self.StandardBand * 0.20
+
+        HighTax = 0
+        if(GrossPay > self.StandardBand):
+            HighTax = (GrossPay - self.StandardBand) * 0.40
+
+        TotalTax = StdTax + HighTax
+
+        NetDeduction = TotalTax - self.TaxCredit
+        NetPay = GrossPay - NetDeduction
+
+        return {
+            'name': self.FirstName+' ' + self.LastName,
+            'Date': date,
+            'Regular Hours Worked': self.RegHours,
+            'Overtime Hours Worked': OT,
+            'Regular Rate': self.HourlyRate,
+            'Overtime Rate': OTRate,
+            'Regular Pay': RegularPay,
+            'Overtime Pay': OTPay,
+            'Gross Pay': GrossPay,
+            'Standard Rate Pay': self.StandardBand,
+            'Higher Rate Pay': round(HigherPay, 2),
+            'Standard Tax': round(StdTax, 2),
+            'Higher Tax': round(HighTax, 2),
+            'Total Tax': TotalTax,
+            'Tax Credit': self.TaxCredit,
+            'Net Deductions': round(NetDeduction, 2),
+            'Net Pay': round(NetPay, 2)
+        }
+
+def computeAllPayment(empFileName, HoursFileName):
+    finalResult = []
+    with open(empFileName, 'r') as fobj:
+        for line in fobj:
+            data = line.split()
+            emp = Employee(data[0], data[1], data[2], data[3],
+                           data[4], data[5], data[6], data[7])
+            with open(HoursFileName, 'r') as hobj:
+                for hline in hobj:
+                    hrs = hline.split()
+                    if(hrs[0] == emp.StaffId):
+                        pay = emp.computePayment(int(hrs[2]), hrs[1])
+                        finalResult.append(pay)
+    print(finalResult)
