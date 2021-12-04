3ea06da1813e76f47db541db85ae5c86fb874b00
Akhil Shirole <shirole.akhil@gmail.com>
2021-12-04 23:41:58 +0000

    Empoy test class

--- main.py
+++ main.py
@@ -75,3 +75,80 @@ def computeAllPayment(empFileName, HoursFileName):
                         pay = emp.computePayment(int(hrs[2]), hrs[1])
                         finalResult.append(pay)
     print(finalResult)
+    def main():
+    computeAllPayment(r"C:\Users\sidds\OneDrive\Desktop\Python_CA_One\Employees.txt", r"C:\Users\sidds\OneDrive\Desktop\Python_CA_One\Hours.txt")
+
+
+class EmployeeTest(unittest.TestCase): #creating a Employeetest class for unit test
+    
+    #Creating three functions named testNetLessEqualGross,testNetLessEqualGrossa,testNetLessEqualGrossb for testing the net pay is less than equal to gross pay  
+    def testNetLessEqualGross(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700) 
+        pi = e.computePayment(1,'31/10/2021')
+        self.assertLessEqual(pi['Net Pay'], pi['Gross Pay'])
+    def testNetLessEqualGrossa(self):
+        e1 = Employee(1234458, 'Shane', 'John', 39, 12, 2.6, 50, 825) 
+        pi1= e1.computePayment(40,'12/11/2021')
+        self.assertLessEqual(pi1['Net Pay'], pi1['Gross Pay'])
+    def testNetLessEqualGrossb(self):
+        e2 = Employee(923696, 'Landon', 'Steve', 39, 12, 0.5, 60, 955) 
+        pi2= e2.computePayment(68,'12/11/2021')
+        self.assertLessEqual(pi2['Net Pay'], pi2['Gross Pay'])
+         
+        #Creating three functions named testOverTimeNotNeg,testOverTimeNotNega and testOverTimeNotNegb for testing Overtime pay is not negative
+    def testOverTimeNotNeg(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(36, '31/10/2021')
+        self.assertGreater(pi['Overtime Pay'], 1)
+    def testOverTimeNotNega(self):
+        e1 = Employee(1234458, 'Shane', 'John', 39, 12, 2.6, 50, 825) 
+        pi1= e1.computePayment(23,'12/11/2021')
+        self.assertGreater(pi1['Overtime Pay'], 1)
+    def testOverTimeNotNegb(self):
+        e2 = Employee(923696, 'Landon', 'Steve', 39, 12, 0.5, 60, 955) 
+        pi2= e2.computePayment(48,'12/11/2021')
+        self.assertGreater(pi2['Overtime Pay'],1)
+        
+        #Create three functions named testRegHourExceed,testRegHourExceeda,testRegHourExceedb for testing Regular Hours Worked cannot exceed hours worked
+    def testRegHourExceed(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(30, '31/10/2021')
+        self.assertLessEqual(pi['Regular Hours Worked'], 30)
+    def testRegHourExceeda(self):
+        e1 = Employee(1234458, 'Shane', 'John', 39, 12, 2.6, 50, 825) 
+        pi1= e1.computePayment(23,'12/11/2021')
+        self.assertLessEqual(pi1['Regular Hours Worked'], 23)
+    def testRegHourExceedb(self):
+        e2 = Employee(923696, 'Landon', 'Steve', 39, 12, 0.5, 60, 955) 
+        pi2= e2.computePayment(48,'12/11/2021')
+        self.assertLessEqual(pi2['Regular Hours Worked'], 48)
+         
+
+        
+        #Create three  functions named testHighTaxNotNeg,testHighTaxNotNega,testHighTaxNotNegb for testing Higher Tax cannot be negative.
+    def testHighTaxNotNeg(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(300, '31/10/2021')
+        self.assertGreater(pi['Higher Tax'],1) 
+    def testHighTaxNotNega(self):
+        e1 = Employee(1234458, 'Shane', 'John', 39, 12, 2.6, 50, 825) 
+        pi1= e1.computePayment(23,'12/11/2021')
+        self.assertGreater(pi1['Higher Tax'],1)
+    def testHighTaxNotNegb(self):
+        e2 = Employee(923696, 'Landon', 'Steve', 39, 12, 0.5, 60, 955) 
+        pi2= e2.computePayment(48,'12/11/2021')
+        self.assertGreater(pi2['Higher Tax'],1) 
+        
+        #Create function testNetPayNotNeg for tetsing Net Pay is not negative
+    def testNetPayNotNeg(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700)
+        pi = e.computePayment(0, '31/10/2021')
+        self.assertGreater(pi['Net Pay'],1)
+    def testNetPayNotNega(self):
+        e1 = Employee(1234458, 'Shane', 'John', 39, 12, 2.6, 50, 825) 
+        pi1= e1.computePayment(23,'12/11/2021')
+        self.assertGreater(pi1['Net Pay'],1)
+    def testNetPayNotNegb(self):
+        e2 = Employee(923696, 'Landon', 'Steve', 39, 12, 0.5, 60, 955) 
+        pi2= e2.computePayment(48,'12/11/2021')
+        self.assertGreater(pi2['Net Pay'],1)
